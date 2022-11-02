2555c59bbbd6fc45622e6e9aeb330a6348b929cf
werserk <werserk@inbox.ru>
2022-11-02 16:04:49 +0300

    speedup of painting and floodfilling

--- src/components/AnnotatorWindow.jsx
+++ src/components/AnnotatorWindow.jsx
@@ -30,11 +30,13 @@ class AnnotatorWindow extends React.Component {
         this.mouseDown = false
         this.mousePosition = {x: 0, y: 0}
         this.currentColor = [0, 255, 0, 0]
-        this.blockColor = [1.0, 1.0, 1.0, 1.0]
+        this.blockColor = [1, 1, 1, 1]
         this.trueColor = [255, 255, 255, 255]
         this.contourFillFlag = cv.RETR_TREE
         this.tolerance = 5
         this.floodFillFlags = 4 | cv.FLOODFILL_FIXED_RANGE | cv.FLOODFILL_MASK_ONLY | 255 << 8
+        this.points = []
+        this.pointIndex = undefined
 
         this.authRequestHeader = { // заголовки авторизации
             name: "Authorization",
@@ -50,6 +52,7 @@ class AnnotatorWindow extends React.Component {
         this.floodFill = this.floodFill.bind(this)
         this.singleFloodFill = this.singleFloodFill.bind(this)
         this.floodFillActivate = this.floodFillActivate.bind(this)
+        this.fillContoursActivate = this.fillContoursActivate.bind(this)
 
         this.canvas = undefined
         this.state = {"ww": this.ww, "wc": this.wc, "update": true}
@@ -88,8 +91,15 @@ class AnnotatorWindow extends React.Component {
         this.mat = new cv.matFromArray(this.shape[1], this.shape[0], cv.CV_8UC1, this.Uint8Image.tolist())
         cv.cvtColor(this.mat, this.mat, cv.COLOR_GRAY2BGR)
 
+        this.blurredImage = new cv.Mat();
+        let ksize = new cv.Size(3, 3);
+        cv.GaussianBlur(this.mat, this.blurredImage, ksize, 0, 0, cv.BORDER_DEFAULT);
+
+        this.maskVisual = this.emptyMask3C.clone()
         this.mask = this.emptyMask3C.clone()
         this.floodMask = this.emptyMask.clone()
+
+        this.allContours = new cv.MatVector()
 
         cv.imshow("canvas", this.mat)
     }
@@ -107,68 +117,35 @@ class AnnotatorWindow extends React.Component {
     // OPENCV FUNCTIONS //
     //////////////////////
 
-    findContours(img, flag) {
+    findContours(mask, flag) {
         // Variables
         let contours = new cv.MatVector();
         let hierarchy = new cv.Mat();
-        let bin_img = new cv.Mat();
+        let binMask = new cv.Mat();
 
         // Transforms
-        cv.cvtColor(img, bin_img, cv.COLOR_RGB2GRAY);
-        cv.threshold(bin_img, bin_img, 1, 254, cv.THRESH_BINARY);
-        cv.findContours(bin_img, contours, hierarchy, flag, cv.CHAIN_APPROX_SIMPLE)
+        cv.cvtColor(mask, binMask, cv.COLOR_RGB2GRAY);
+        cv.threshold(binMask, binMask, 2, 255, cv.THRESH_BINARY);
+        cv.findContours(binMask, contours, hierarchy, flag, cv.CHAIN_APPROX_SIMPLE)
 
         // Clear memory
-        bin_img.delete()
+        binMask.delete()
         hierarchy.delete()
 
         return contours
     }
 
-    getContours(mask, color) {
-        let contours = this.findContours(mask, this.contourFillFlag)
-        let viz = this.emptyMask3C.clone()
-        cv.drawContours(viz, contours, -1, color, -1)
-        return [viz, contours]
+    getContours(mask) {
+        // let contours = this.findContours(mask, this.contourFillFlag
+        let contours = this.findContours(mask, cv.RETR_TREE)
+        return contours
     }   
-
-    applyPerPixel(src, mask, dst, func) {
-        for (let i = 0; i < src.rows; i++) {
-            for (let j = 0; j < src.cols; j++) {
-                func(src.ucharPtr(i, j), mask.ucharPtr(i, j), dst.ucharPtr(i, j))
-            }
-        }
-    }
-
-    createBooleanMask(src, boolFunc) {
-        let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC1)
-        for (let i = 0; i < src.rows; i++) {
-            for (let j = 0; j < src.cols; j++) {
-                if (boolFunc(src.ucharPtr(i, j))) {
-                    dst.ucharPtr(i, j)[0] = this.trueColor[0]
-                    dst.ucharPtr(i, j)[1] = this.trueColor[0]
-                    dst.ucharPtr(i, j)[2] = this.trueColor[0]
-                    dst.ucharPtr(i, j)[3] = this.trueColor[0]
-                }
-            }
-        }
-        return dst
-    }
-
-    dividePixelByValue3C(src, mask, dst, value) {
-        dst[0] = src[0] / value
-        dst[1] = src[1] / value
-        dst[2] = src[2] / value
-        dst[3] = src[3] / value
-    }
 
     singleFloodFill(image, x, y, tolerance){
         // Создаём специальную временную пустую маску для заливки
         let floodMaskTemp = cv.Mat.zeros(image.rows + 2, image.cols + 2, cv.CV_8UC1)
 
         // Процесс заливки
-        // console.log('flags', this.floodFillFlags)
-        // console.log('floodMaskTemp', floodMaskTemp)
         cv.floodFill(image, floodMaskTemp, new cv.Point(x, y), this.currentColor, image, tolerance, tolerance, this.floodFillFlags)
 
         // Обрезаем специальную маску (такова специфика функции)
@@ -182,53 +159,25 @@ class AnnotatorWindow extends React.Component {
         this.floodMask = this.emptyMask.clone()
 
         // Получаем контуры всех объектов
-        this.allContours = this.findContours(this.mask, cv.RETR_TREE)
-
         const st = this.tolerance
         const tolerance = [st, st, st, st] // Максимальное отклонение пикселей при заливки
 
-        this.imageTemp = cv.Mat.zeros(this.shape[1], this.shape[0], cv.CV_32FC3) // Изображение для заливки
-        const funcDivideBy1000 = (src, mask, dst) => this.dividePixelByValue3C(src, mask, dst, 1000) // Функция для деления изображения на 1000
-        this.blurredImage = this.mat.clone() // Изображение, по которому будем смотреть заливку
-        this.applyPerPixel(this.blurredImage, this.blurredImage, this.imageTemp, funcDivideBy1000) // Теперь image = blurredImage / 1000
-        this.imageTemp = this.mat.clone()
-
-        // Создаём запретную зону для floodfill (красный цвет при разметке)
-        const isBlockPixel = (pixel) => pixel !== this.blockColor
-        this.criterionMask = this.createBooleanMask(this.blurredImage, isBlockPixel)
-        const makeBlockColor = (src, mask, dst) => {
-            if (mask === this.trueColor) {
-                dst[0] = this.blockColor[0]
-                dst[1] = this.blockColor[1]
-                dst[2] = this.blockColor[2]
-                dst[3] = this.blockColor[3]
-            }
-        }
-        console.log('this.imageTemp_1', this.imageTemp)
-        this.applyPerPixel(this.imageTemp, this.criterionMask, this.imageTemp, makeBlockColor) // <=> image[self.mask == 2] = 1.0
-        console.log('this.imageTemp_2', this.imageTemp)
+        // Необходимо создать запретную зону для floodfill
+        this.imageTemp = this.blurredImage.clone()
 
         for (let i=0; this.allContours.get(i) !== undefined; i++) {  // Итерируемся для каждого объекта
             let contours = this.allContours.get(i)
-            for (let j=0; j < contours.rows; j++) { // Итерируемся для каждой пары координат
+            for (let j=0; j < contours.rows; j+=5) { // Итерируемся для каждой пары координат
                 let x = contours.data32S[j * 2]  // Нас интересуют только пары координат
                 let y = contours.data32S[j * 2 + 1]
                 this.floodMaskTemp = this.singleFloodFill(this.imageTemp, x, y, tolerance)  // Заливка
                 cv.bitwise_or(this.floodMask, this.floodMaskTemp, this.floodMask)  // Объединяем с ранее созданными масками
             }
         }
-        //     // Такую же процедуру сделаем для центра объекта
-        //     centroid = contours.mean(axis=1).mean(axis=0)  // Находим примерный центр
-        //     xc, yc = int(centroid[0]), int(centroid[1])  // Переводим в int
-        //     if self.positive_mask[xc][yc] == 1:  // Если центр является размеченным (а то может быть кольцо)
-        //         flood_mask = self._single_floodfill(image, xc, yc, tolerance)  // Заливка
-        //         self.flood_mask = cv2.bitwise_or(self.flood_mask, flood_mask)  // Объединяем с ранее созданными масками
-        // }
-        // self.flood_mask = remove_small_dots(self.flood_mask)  // Убираем мелкие точки
-        // self._update_image()
 
-        // Clear memory
-        // image.delete()
+        let floodMask = new cv.Mat()
+        cv.cvtColor(this.floodMask, floodMask, cv.COLOR_GRAY2BGR)
+        cv.bitwise_or(this.mask, floodMask, this.maskVisual) 
     }
 
     pickMax(src, mask, dst) {
@@ -240,39 +189,34 @@ class AnnotatorWindow extends React.Component {
 
     updateImage() {
         // Combine mask and image
-        let image = new cv.Mat();
-        let mask = this.mask.clone()
+        console.time('updateImage')
 
-        // НЕОБХОДИМО СКЛЕИТЬ 2 МАСКИ
-        let floodMask = new cv.Mat()
-        cv.cvtColor(this.floodMask, floodMask, cv.COLOR_GRAY2BGR)
-        cv.bitwise_or(this.mask, floodMask, mask) 
-
-        let [viz, contours] = this.getContours(mask, this.currentColor)
-        cv.addWeighted(this.mat, 0.75, viz, 0.25, 0, image)
-        cv.drawContours(image, contours, -1, this.currentColor, 1, cv.LINE_AA)
-        
-        // TODO: сохранение на оригинальной маске, а не только на визуализации!!
-        // this.mask = viz.clone()
+        let viz = new cv.Mat();
+        this.allContours = this.getContours(this.maskVisual, this.currentColor)
+        let maskVisualTemp = this.maskVisual.clone()
+       
+        cv.drawContours(maskVisualTemp, this.allContours, -1, this.currentColor, -1, cv.LINE_AA)
+        cv.addWeighted(this.mat, 0.75, maskVisualTemp, 0.25, 0, viz)
+        cv.drawContours(viz, this.allContours, -1, this.currentColor, 1, cv.LINE_AA)
 
         // Update canvas
-        cv.circle(image, this.mousePosition, this.brushSize, [255, 255, 255, 0], 1, cv.LINE_AA)
-        cv.imshow("canvas", image)
+        cv.circle(viz, this.mousePosition, this.brushSize, [255, 255, 255, 0], 1, cv.LINE_AA)
+        cv.imshow("canvas", viz)
 
         // Clear memory
-        image.delete()
         viz.delete()
-        mask.delete()
-        contours.delete()
+        maskVisualTemp.delete()
+        console.timeEnd('updateImage')
     }
 
     drawCircle(color, thickness) {
-        // Draw on mask
         cv.circle(this.mask, this.mousePosition, this.brushSize, color, thickness, cv.LINE_AA)
+        cv.circle(this.maskVisual, this.mousePosition, this.brushSize, color, thickness, cv.LINE_AA)
     }
 
     clearMask() {
         this.mask = this.emptyMask3C.clone()
+        this.maskVisual = this.emptyMask3C.clone()
         this.updateImage()
     }
 
@@ -285,21 +229,22 @@ class AnnotatorWindow extends React.Component {
         this.updateImage()
     }
 
+    fillContoursActivate() {
+        if (this.contourFillFlag === cv.RETR_EXTERNAL) {
+            this.contourFillFlag = cv.RETR_TREE
+        }
+        else {
+            this.contourFillFlag = cv.RETR_EXTERNAL
+        }
+        this.updateImage()
+    }
+
     mouseCallback() {       
         if (this.mouseDown) {
             if (this.tool === "Paint") {
                 this.canvas.onmousemove = (e) => {
                     this.mousePosition = {x: e.clientX - e.target.offsetLeft, y: e.clientY - e.target.offsetTop} // mouse position
                     this.drawCircle(this.currentColor, -1)
-                    this.contourFillFlag = cv.RETR_TREE
-                    this.updateImage()
-                }
-            }
-            if (this.tool === "Contour paint") {
-                this.canvas.onmousemove = (e) => {
-                    this.mousePosition = {x: e.clientX - e.target.offsetLeft, y: e.clientY - e.target.offsetTop} // mouse position
-                    this.drawCircle(this.currentColor, -1)
-                    this.contourFillFlag = cv.RETR_EXTERNAL
                     this.updateImage()
                 }
             }
@@ -337,7 +282,7 @@ class AnnotatorWindow extends React.Component {
                 <button onClick={() => this.setTool("Paint")}>Paint</button>
                 <br />
                 <br />
-                <button onClick={() => this.setTool("Contour paint")}>Contour paint</button>
+                <button onClick={this.fillContoursActivate}>Fill contours</button>
                 <br />
                 <br />
                 <button onClick={() => this.setTool("Eraser")}>Eraser</button>
