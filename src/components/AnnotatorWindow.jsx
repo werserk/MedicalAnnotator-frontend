13ee97705035e29813d0d59d4e7ac40e7d04eedc
Gvatest <ganjatriller@gmail.com>
2022-11-05 19:07:45 +0300

    slices iteration demo

--- src/components/AnnotatorWindow.jsx
+++ src/components/AnnotatorWindow.jsx
@@ -79,15 +79,24 @@ class AnnotatorWindow extends React.Component {
         const config = {
             headers: {
                 "Authorization": this.authRequestHeader.value
+            },
+            params: {
+                axial_id: 6,
+                coronal_id: 2,
+                saggital_id: 6,
             }
         }
         axios.get(this.fetchUrl, config).then((response) => {
-            const paths = response.data["paths"][0]
-            const urls = []
-            for (let url of paths) {
-                urls.push(BASE_URL + "media/" + "?" + "path=" + url)
+            let paths
+            if (response.data["many"]) {
+                console.log(response.data)
+                console.log(response.data["axial_slices_paths"])
+                paths = response.data["axial_slices_paths"][0]
             }
-            this.app.loadURLs(urls, {"requestHeaders": [this.authRequestHeader]})
+            else {
+                paths = response.data["path"]
+            }
+            this.app.loadURLs([BASE_URL + paths], {"requestHeaders": [this.authRequestHeader]})
             this.app.addEventListener('loadend', () => {
                 const CV_NJLoadingHandle = setInterval(() => {
                     if (cv && nj) {
@@ -119,7 +128,6 @@ class AnnotatorWindow extends React.Component {
 
         this.buffer = this.image.getBuffer() 
         this.Uint8Image = apply_windowing(new Float32Array(this.buffer), this.wc, this.ww)
-        console.log(this.Uint8Image)
 
         this.emptyMask = cv.Mat.zeros(this.shape[1], this.shape[0], cv.CV_8UC1)
         this.emptyMask3C = cv.Mat.zeros(this.shape[1], this.shape[0], cv.CV_8UC3)
